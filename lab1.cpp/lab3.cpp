8c41cdcf7834f0b6bcf8c7ae8d476ff35dd344c2
Valera346 <valera346e@gmail.com>
2024-12-06 00:49:15 +0200

    zadacha

---
+++ lab1.cpp/lab3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+ int main()
+{
+	double num1, num2;
+
+	std::cout << "persshe chuslo" << std::endl;
+
+	std::cin >> num1;
+
+	std::cout << "dryge chuslo" << std::endl;
+
+	std::cin >> num2;
+	std::cout << fixet << setprecision (20);
+
+	std::cout << "syma  " << num1 + num2 << std::endl;
+	std::cout << "reznusa  " << num1 - num2 <<std::endl;
+	std::cout << "dobutok  " << num1 * num2 << std::endl;
+
+	if (num2 !=0){
+		std::cout <<  "chastka" << num1 / num2 << std::cout 
+}
+else {std::cout <<" error 404" <<std::endl;
+}
+	return 0;
+	}
+
