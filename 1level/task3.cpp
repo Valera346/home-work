68680dd577ac9b2b288950f96a6ae5f2a231d0b2
Valera346 <valera346e@gmail.com>
2025-02-17 16:47:57 +0200

    змінено:       task3.cpp

--- 1level/task3.cpp
+++ 1level/task3.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(){
 
 int a, b, c;
-	cout<< " dani dla paralelograma A, B, c,de c vusota" << endl;
-		cout << "dani dla A, B, C";
+	cout<< " dani dla paralelograma A, B, c,de c vusota" << endl << endl;
+		cout << "dani dla A, B, C"<< endl;
 		cin >> a >> b >> c;
 			cout << "plohcha storonu palalelograma" << endl << 2 *(a * b + b * c + a * c ) << endl;
 			cout << "obem paralelepipeda" << endl << a*b*c << endl; 
