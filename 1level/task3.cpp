4fe1ce7ca91dd6d639900f7f0d125c0164cab92b
Valera346 <valera346e@gmail.com>
2025-02-17 16:44:18 +0200

    змінено:       a.out 	новий файл:    task3.cpp

---
+++ 1level/task3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int a, b, c;
+	cout<< " dani dla paralelograma A, B, c,de c vusota" << endl;
+		cout << "dani dla A, B, C";
+		cin >> a >> b >> c;
+			cout << "plohcha storonu palalelograma" << endl << 2 *(a * b + b * c + a * c ) << endl;
+			cout << "obem paralelepipeda" << endl << a*b*c << endl; 
+
+}
