3345b5a737b6634310c04cba241d14972cd151ca
Valera346 <valera346e@gmail.com>
2025-02-17 20:49:24 +0200

    новий файл:    task4.cpp

---
+++ 1level/task4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	double  pi=3.14, R;
+	cout << "radius kola" << endl;
+        cin >> R;
+	
+	double radius = pi*R*R;
+	double L= 2*pi*R;	
+		cout << radius << endl << L << endl;
+		
+	}	
+
+
