c6607241404e76ec6b5cfee82098ee583177d16c
Valera346 <valera346e@gmail.com>
2025-02-17 16:06:57 +0200

    змінено:       a.out 	новий файл:    task2.cpp

---
+++ 1level/task2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a, b;
+	cout  <<  "pershe chslo a " << endl;
+	cin >>  a;
+	cout << "druge chuslo" << endl;
+	cin >> b;
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+
+}
