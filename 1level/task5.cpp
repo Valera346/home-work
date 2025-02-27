e26f3a34a5dfcfcb7d891d89d9c2daed5854bbf5
Valera346 <165339512+Valera346@users.noreply.github.com>
2025-02-27 13:12:50 +0200

    Update task5.cpp

--- 1level/task5.cpp
+++ 1level/task5.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-using namespace std; 
  int main(){
 	float R,P;
 	int T;
-		cout << "syma groshe"  << endl;
-		cin >> P;
-		cout << "period" << endl;
-		cin >> T;
-		cout << "procent"<< endl;
-		cin >> R;
+		std::cout << "syma groshe"  << std::endl;
+		std::cin >> P;
+		std::cout << "period" << std::endl;
+		std::cin >> T;
+		std::cout << "procent"<< std::endl;
+		std::cin >> R;
 
 	float I = (P*T*R)/100;
 		
-		cout << "procent" << I << endl;
-		cout  << static_cast<int>(I) << endl;
+		std::cout << "procent" << I << std::endl;
+		std::cout  << static_cast<int>(I) << std::endl;
 
  }
 
