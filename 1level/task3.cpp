4a91f790db4821022d778979866fdae5d855b51b
Valera346 <165339512+Valera346@users.noreply.github.com>
2025-02-27 13:10:46 +0200

    Update task3.cpp

--- 1level/task3.cpp
+++ 1level/task3.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-using namespace std;
 
 int main(){
 
 int a, b, c;
-	cout<< " dani dla paralelograma A, B, c,de c vusota" << endl << endl;
-		cout << "dani dla A, B, C"<< endl;
-		cin >> a >> b >> c;
-			cout << "plohcha storonu palalelograma" << endl << 2 *(a * b + b * c + a * c ) << endl;
-			cout << "obem paralelepipeda" << endl << a*b*c << endl; 
+	std::cout<< " dani dla paralelograma A, B, c,de c vusota" << std::endl << std::endl;
+		std::cout << "dani dla A, B, C"<< std::endl;
+		std::cin >> a >> b >> c;
+			std::cout << "plohcha storonu palalelograma" << std::endl << 2 *(a * b + b * c + a * c ) << std::endl;
+			std::cout << "obem paralelepipeda" << std::endl << a*b*c << std::endl; 
 
 }
