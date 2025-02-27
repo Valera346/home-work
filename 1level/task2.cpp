2b21e31b1b320fd41b56d18163615220a7825f7b
Valera346 <165339512+Valera346@users.noreply.github.com>
2025-02-27 13:09:05 +0200

    Update task2.cpp

--- 1level/task2.cpp
+++ 1level/task2.cpp
@@ -2,12 +2,12 @@
 
 int main(){
 	int a, b;
-	std::cout  <<  "pershe chslo a " << endl;
+	std::cout  <<  "pershe chslo a " << std::endl;
 	std::cin >>  a;
-	std::cout << "druge chuslo" << endl;
+	std::cout << "druge chuslo" << std::endl;
 	std::cin >> b;
-	std::cout << a + b << endl;
-	std::cout << a - b << endl;
-	std::cout << a * b << endl;
+	std::cout << a + b << std::endl;
+	std::cout << a - b << std::endl;
+	std::cout << a * b << std::endl;
 
 }
